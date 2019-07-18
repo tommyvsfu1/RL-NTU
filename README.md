32ac824ad329e7b412712249c4d61e9b8872b673
tommy <tommy558788@gmail.com>
2019-07-18 11:20:52 +0800

    commit

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # RL-NTU
 
-
+## PG - PPO  
+### PPO paper
+https://arxiv.org/pdf/1707.06347.pdf
 
 ## DQN - BreakoutNoFrameskip-v4  
 ### Preprocessing
 Use deepmind wrapper to do some preprocessing of game images, check this awesome blog [Frame Skipping and Pre-Processing for Deep Q-Networks on Atari 2600 Games
 ](https://danieltakeshi.github.io/2016/11/25/frame-skipping-and-preprocessing-for-deep-q-networks-on-atari-2600-games/).
+
+
+
+## references
+https://github.com/AdrianHsu/breakout-Deep-Q-Network
+https://github.com/JasonYao81000/MLDS2018SPRING
+https://github.com/nikhilbarhate99/PPO-PyTorch/blob/master/PPO.py
--- RL_mlds_hw4/hw4/agent_dir/agent_pg.py
+++ RL_mlds_hw4/hw4/agent_dir/agent_pg.py
@@ -10,6 +10,126 @@ import skimage
 seed = 9500
 np.random.seed(seed)
 
+
+class Memory:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
+    
+    def clear_memory(self):
+        del self.actions[:]
+        del self.states[:]
+        del self.logprobs[:]
+        del self.rewards[:]
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim, n_latent_var):
+        super(ActorCritic, self).__init__()
+        self.affine = torch.nn.Linear(state_dim, n_latent_var)
+        
+        # actor
+        self.action_layer = torch.nn.Sequential(
+                torch.nn.Linear(state_dim, n_latent_var),
+                torch.nn.Tanh(),
+                torch.nn.Linear(n_latent_var, n_latent_var),
+                torch.nn.Tanh(),
+                torch.nn.Linear(n_latent_var, action_dim),
+                torch.nn.Softmax(dim=-1)
+                )
+        
+        # critic
+        self.value_layer = torch.nn.Sequential(
+                torch.nn.Linear(state_dim, n_latent_var),
+                torch.nn.Tanh(),
+                torch.nn.Linear(n_latent_var, n_latent_var),
+                torch.nn.Tanh(),
+                torch.nn.Linear(n_latent_var, 1)
+                )
+        
+    def forward(self):
+        raise NotImplementedError
+        
+    def act(self, state, memory):
+        state = torch.from_numpy(state).float().to(device) 
+        action_probs = self.action_layer(state)
+        dist = torch.distributions.Categorical(action_probs)
+        action = dist.sample()
+        
+        memory.states.append(state)
+        memory.actions.append(action)
+        memory.logprobs.append(dist.log_prob(action))
+        
+        return action.item()
+    
+    def evaluate(self, state, action):
+        action_probs = self.action_layer(state)
+        dist = torch.distributions.Categorical(action_probs)
+        
+        action_logprobs = dist.log_prob(action)
+        dist_entropy = dist.entropy()
+        
+        state_value = self.value_layer(state)
+        
+        return action_logprobs, torch.squeeze(state_value), dist_entropy
+    
+
+class PPO:
+    def __init__(self, state_dim, action_dim, n_latent_var, lr, betas, gamma, K_epochs, eps_clip):
+        self.lr = lr
+        self.betas = betas
+        self.gamma = gamma
+        self.eps_clip = eps_clip
+        self.K_epochs = K_epochs
+        
+        self.policy = ActorCritic(state_dim, action_dim, n_latent_var).to(device)
+        self.optimizer = torch.optim.Adam(self.policy.parameters(),
+                                              lr=lr, betas=betas)
+        self.policy_old = ActorCritic(state_dim, action_dim, n_latent_var).to(device)
+        
+        self.MseLoss = torch.nn.MSELoss()
+    
+    def update(self, memory):   
+        # Monte Carlo estimate of state rewards:
+        rewards = []
+        discounted_reward = 0
+        for reward in reversed(memory.rewards):
+            discounted_reward = reward + (self.gamma * discounted_reward)
+            rewards.insert(0, discounted_reward)
+        
+        # Normalizing the rewards:
+        rewards = torch.tensor(rewards).to(device)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+        
+        # convert list to tensor
+        old_states = torch.stack(memory.states).to(device).detach()
+        old_actions = torch.stack(memory.actions).to(device).detach()
+        old_logprobs = torch.stack(memory.logprobs).to(device).detach()
+        
+        # Optimize policy for K epochs:
+        for _ in range(self.K_epochs):
+            # Evaluating old actions and values :
+            logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
+            
+            # Finding the ratio (pi_theta / pi_theta__old):
+            ratios = torch.exp(logprobs - old_logprobs.detach())
+                
+            # Finding Surrogate Loss:
+            advantages = rewards - state_values.detach()
+            surr1 = ratios * advantages
+            surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages
+            loss = -torch.min(surr1, surr2) + 0.5*self.MseLoss(state_values, rewards) - 0.01*dist_entropy
+            
+            # take gradient step
+            self.optimizer.zero_grad()
+            loss.mean().backward()
+            self.optimizer.step()
+        
+        # Copy new weights into old policy:
+        self.policy_old.load_state_dict(self.policy.state_dict())
+        
+
 def prepro(o,image_size=[80,80]):
     """
     Call this function to preprocess RGB image to grayscale image if necessary
@@ -41,7 +161,6 @@ def prepro(o,image_size=[80,80]):
     """
 
 
-
 class Agent_PG(Agent):
     def __init__(self, env, args):
         """
@@ -57,15 +176,19 @@ class Agent_PG(Agent):
         ##################
         # YOUR CODE HERE #
         self.init_action = None
-        print("Environment info:")
+        print("Setting Environment ...")
+        self.env = env
+        self.env.seed(seed)
+
+        print("Environment info ...")
         print("  action space",self.env.get_action_space())
         print("  obeservation space",self.env.get_observation_space())
         
         print("-- Debug -- ")
         print("def discount rewards: in debug_agent_pg.py : True")
+        
+        
         # Model : Neural Network
-        self.env = env
-        self.env.seed(seed)
         self.device = torch.device('cuda')
         print("using device",self.device)
         D_in, H, D_out = 80*80, 256, 3
